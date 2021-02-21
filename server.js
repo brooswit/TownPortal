0d61fa2933c9df9d1e8246d2d52add965963ba45
John Winstead <jwinstead@launchdarkly.com>
2021-02-21 23:05:00 +0000

    restructure, add terra

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ let entityCollection = db.collection('entities');
 app.use(express.static('dist'));
 
 let states = {};
+
 io.on('connection', (socket) => {
     states[socket.id] = {};
     let prefix = `[user@${socket.id}]`;
@@ -59,80 +60,174 @@ function oneOf(array) {
     return Array.isArray(array) ? array[Math.floor(Math.random() * array.length)] : array;
 }
 
-function grassController(previousStage, currentStage, nextStages) {
-    return async (doc) => {
-        let data = doc.data();
+// function grassController(previousStage, currentStage, nextStages) {
+//     return async (doc) => {
+//         let data = doc.data();
 
-        let firstStage = "light grass";
+//         let firstStage = "light grass";
 
-        if(Math.random() < 0.20) {
-            doc.update({
-                classname: previousStage
-            });
-        } else {
-            let offsets = [{x: 0, y: 1,},{x: 0,y: -1,},{x: 1,y: 0,},{x: -1,y: 0,},];
-            let offset = oneOf(offsets);
+//         if(Math.random() < 0.20) {
+//             doc.update({
+//                 classname: previousStage
+//             });
+//         } else {
+//             let offsets = [{x: 0, y: 1,},{x: 0,y: -1,},{x: 1,y: 0,},{x: -1,y: 0,},];
+//             let offset = oneOf(offsets);
 
-            let snapshot = await entityCollection
-                .where('x', '==', data.x+offset.x)
-                .where('y', '==', data.y+offset.y)
-                .get();
+//             let snapshot = await entityCollection
+//                 .where('x', '==', data.x+offset.x)
+//                 .where('y', '==', data.y+offset.y)
+//                 .get();
 
-            if (snapshot.empty) {
-                let entityData = {
-                    classname: firstStage,
-                    x: data.x+offset.x,
-                    y: data.y+offset.y
-                };
-                let res = await entityCollection.add(entityData);
+//             if (snapshot.empty) {
+//                 let entityData = {
+//                     classname: firstStage,
+//                     x: data.x+offset.x,
+//                     y: data.y+offset.y
+//                 };
+//                 let res = await entityCollection.add(entityData);
 
-                console.log(res.id, entityData);
+//                 console.log(res.id, entityData);
 
-                return;
-            }
+//                 return;
+//             }
 
-            snapshot = await entityCollection
-                .where('x', '==', data.x+offset.x)
-                .where('y', '==', data.y+offset.y)
-                .where('classname', '==', "dirt")
-                .get();
+//             snapshot = await entityCollection
+//                 .where('x', '==', data.x+offset.x)
+//                 .where('y', '==', data.y+offset.y)
+//                 .where('classname', '==', "dirt")
+//                 .get();
 
-            if (!snapshot.empty) {
-                snapshot.forEach(doc => {
-                    doc.update({
-                        classname: firstStage
+//             if (!snapshot.empty) {
+//                 snapshot.forEach(doc => {
+//                     doc.update({
+//                         classname: firstStage
+//                     });
+//                 });
+//                 return;
+//             }
+
+//             snapshot = await entityCollection
+//                 .where('x', '==', data.x+offset.x)
+//                 .where('y', '==', data.y+offset.y)
+//                 .where('classname', '==', currentStage)
+//                 .get();
+
+//             if (!snapshot.empty) {
+//                 snapshot.forEach(doc => {
+//                     let patch = {
+//                         classname: oneOf(nextStages)
+//                     };
+//                     doc.update(patch);
+//                     console.log(patch);
+//                 });
+//                 return;
+//             }
+//         }
+//     }
+// }
+
+// let controllers = {
+//     "light grass": grassController("dirt", "light grass", "grass"),
+//     "grass": grassController("light grass", "grass", "healthy grass"),
+//     "healthy grass": grassController("grass", "healthy grass", ["flower", "overgrowth"]),
+//     "flower": grassController("flower", "flower", "healthy grass"),
+//     "overgrowth": grassController("healthy grass", "overgrowth", ["healthy grass", "bush"]),
+//     "bush": grassController("bush", "healthy grass", "overgrowth"),
+// }
+
+// function grassController(previousStage, currentStage, nextStages) {
+
+let controllers = {
+    "terra": async(doc) => {
+        const data = doc.data();
+        const self = makeEntity(data);
+        const game = {
+            doc,
+            find: async(patch) => {
+                const results = [];
+                let snapshot = entityCollection;
+                for (key in patch) {
+                    snapshot = snapshot.where(key, '==', patch[key]);
+                }
+                snapshot = snapshot.where('deleted', '!=', true);
+                snapshot = await snapshot.get();
+                if (!snapshot.empty) {
+                    snapshot.forEach(doc => {
+                        await makeEntity(doc.data());
                     });
                 });
-                return;
+                return results;
+            },
+            findOne: async(patch) => {
+                return (await game.find(patch))[0];
+            },
+            spawn: async(patch) => {
+                const doc = await entityCollection.add(patch);
+                return makeEntity(doc.data());
+            }
+        }
+
+        async function makeEntity(data){
+            Object.assign({}, data, {
+                update: async (patch) => {
+                    doc.update(patch);
+                }
+            });
+        }
+
+        await think(self, game);
+
+        async function think(self, game) {
+            let land = null;
+            
+            if (!(land = await game.findOne({
+                "classname": "land",
+                "x": self.x,
+                "y": self.y
+            }))) {
+                land = await game.spawn({
+                    "classname": "land",
+                    "x": self.x,
+                    "y": self.y,
+                    "height": 0
+                });
             }
 
-            snapshot = await entityCollection
-                .where('x', '==', data.x+offset.x)
-                .where('y', '==', data.y+offset.y)
-                .where('classname', '==', currentStage)
-                .get();
+            await land.update({
+                "height": land.height + Math.random()
+            });
 
-            if (!snapshot.empty) {
-                snapshot.forEach(doc => {
-                    let patch = {
-                        classname: oneOf(nextStages)
-                    };
-                    doc.update(patch);
-                    console.log(patch);
-                });
-                return;
+            const moveChance = 1/3;
+            const shouldMove = Math.random() < moveChance;
+            if (shouldMove) {
+                const NORTH = 0;
+                const EAST = 1;
+                const SOUTH = 2;
+                const WEST = 3;
+                const DIRECTIONS = [NORTH, EAST, SOUTH, WEST];
+
+                const moveDirection = oneOf(DIRECTIONS)
+                if (moveDirection === NORTH) {
+                    self.update({
+                        y: self.y+1
+                    });
+                } else if (moveDirection === EAST) {
+                    self.update({
+                        x: self.x+1
+                    });
+                } else if (moveDirection === SOUTH) {
+                    self.update({
+                        y: self.y-1
+                    });
+                } else if (moveDirection === WEST) {
+                    self.update({
+                        x: self.x-1
+                    });
+                }
             }
         }
     }
-}
-
-let controllers = {
-    "light grass": grassController("dirt", "light grass", "grass"),
-    "grass": grassController("light grass", "grass", "healthy grass"),
-    "healthy grass": grassController("grass", "healthy grass", ["flower", "overgrowth"]),
-    "flower": grassController("flower", "flower", "healthy grass"),
-    "overgrowth": grassController("healthy grass", "overgrowth", ["healthy grass", "bush"]),
-    "bush": grassController("bush", "healthy grass", "overgrowth"),
 }
 
 async function run() {
