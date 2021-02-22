436903a8e4301d7a423af04e073d57755194b160
John Winstead <jwinstead@launchdarkly.com>
2021-02-22 18:30:44 +0000

    fix

--- server.js
+++ server.js
@@ -17,6 +17,47 @@ ldClient.on('update', (param) => {
   console.log(`a flag was changed: ${JSON.stringify(param)}`);
 });
 let entityCollection = db.collection('entities');
+const game = {
+    find: async(patch) => {
+        const results = [];
+        let snapshots = entityCollection;
+        for (key in patch) {
+            snapshots = snapshots.where(key, '==', patch[key]);
+        }
+        snapshots = await snapshots.get();
+        let promises = [];
+        if (!snapshots.empty) {
+            snapshots.forEach(async snapshot => {
+                promises.push(makeEntity(snapshot.ref));
+            });
+        };
+        return await Promise.all(promises);
+    },
+    findOne: async(patch) => {
+        return (await game.find(patch))[0];
+    },
+    spawn: async(patch) => {
+        const ref = await entityCollection.add(patch);
+        return await makeEntity(ref);
+    }
+}
+
+async function makeEntity(ref) {
+    const snapshot = await ref.get();
+    const data = snapshot.data();
+    const self = {};
+    const proto = {
+        id: ref.id,
+        update: async (patch) => {
+            await ref.update(patch);
+            Object.assign(self, patch, proto);
+        },
+        delete: async () => {
+            await ref.delete();
+        }
+    }
+    return Object.assign(self, data, proto);
+}
 
 app.use(express.static('dist'));
 
@@ -60,175 +101,69 @@ function oneOf(array) {
     return Array.isArray(array) ? array[Math.floor(Math.random() * array.length)] : array;
 }
 
-// function grassController(previousStage, currentStage, nextStages) {
-//     return async (doc) => {
-//         let data = doc.data();
-
-//         let firstStage = "light grass";
-
-//         if(Math.random() < 0.20) {
-//             doc.update({
-//                 classname: previousStage
-//             });
-//         } else {
-//             let offsets = [{x: 0, y: 1,},{x: 0,y: -1,},{x: 1,y: 0,},{x: -1,y: 0,},];
-//             let offset = oneOf(offsets);
-
-//             let snapshot = await entityCollection
-//                 .where('x', '==', data.x+offset.x)
-//                 .where('y', '==', data.y+offset.y)
-//                 .get();
-
-//             if (snapshot.empty) {
-//                 let entityData = {
-//                     classname: firstStage,
-//                     x: data.x+offset.x,
-//                     y: data.y+offset.y
-//                 };
-//                 let res = await entityCollection.add(entityData);
-
-//                 console.log(res.id, entityData);
-
-//                 return;
-//             }
-
-//             snapshot = await entityCollection
-//                 .where('x', '==', data.x+offset.x)
-//                 .where('y', '==', data.y+offset.y)
-//                 .where('classname', '==', "dirt")
-//                 .get();
-
-//             if (!snapshot.empty) {
-//                 snapshot.forEach(doc => {
-//                     doc.update({
-//                         classname: firstStage
-//                     });
-//                 });
-//                 return;
-//             }
-
-//             snapshot = await entityCollection
-//                 .where('x', '==', data.x+offset.x)
-//                 .where('y', '==', data.y+offset.y)
-//                 .where('classname', '==', currentStage)
-//                 .get();
-
-//             if (!snapshot.empty) {
-//                 snapshot.forEach(doc => {
-//                     let patch = {
-//                         classname: oneOf(nextStages)
-//                     };
-//                     doc.update(patch);
-//                     console.log(patch);
-//                 });
-//                 return;
-//             }
-//         }
-//     }
-// }
-
-// let controllers = {
-//     "light grass": grassController("dirt", "light grass", "grass"),
-//     "grass": grassController("light grass", "grass", "healthy grass"),
-//     "healthy grass": grassController("grass", "healthy grass", ["flower", "overgrowth"]),
-//     "flower": grassController("flower", "flower", "healthy grass"),
-//     "overgrowth": grassController("healthy grass", "overgrowth", ["healthy grass", "bush"]),
-//     "bush": grassController("bush", "healthy grass", "overgrowth"),
-// }
-
-// function grassController(previousStage, currentStage, nextStages) {
-
 let controllers = {
-    "land": async (snapshot) => {},
-    "terra": async(snapshot) => {
-        console.log(`thinkin bout terra`);
-        const self = await makeEntity(snapshot.ref);
-        const data = snapshot.data();
-        const game = {
-            find: async(patch) => {
-                const results = [];
-                let snapshots = entityCollection;
-                for (key in patch) {
-                    snapshots = snapshots.where(key, '==', patch[key]);
-                }
-                snapshots = snapshots.where('deleted', '!=', true);
-                snapshots = await snapshots.get();
-                let promises = [];
-                if (!snapshots.empty) {
-                    snapshots.forEach(async snapshot => {
-                        promises.push(makeEntity(snapshot.ref));
-                    });
-                };
-                return await Promise.all(promises);;
-            },
-            findOne: async(patch) => {
-                return (await game.find(patch))[0];
-            },
-            spawn: async(patch) => {
-                const ref = await entityCollection.add(patch);
-                return await makeEntity(ref);
-            }
-        }
+    "land": async (self, game) => {
+        const lands = await game.find({
+            "classname": "land",
+            "x": self.x,
+            "y": self.y
+        });
 
-        async function makeEntity(ref){
-            const snapshot = await ref.get();
-            const data = snapshot.data();
-            return Object.assign({}, data, {
-                update: async (patch) => {
-                    await ref.update(patch);
-                }
+        for (land of lands) {
+            if (land.id === self.id) continue;
+            await self.update({
+                height: self.height + land.height
             });
+            await land.delete();
         }
-
-        await think(self, game);
-
-        async function think(self, game) {
-            let land = null;
-            
-            if (!(land = await game.findOne({
+    },
+    "terra": async(self, game) => {
+        console.log(`Terra is ${self.id}`);
+        let land = null;
+        
+        if (!(land = await game.findOne({
+            "classname": "land",
+            "x": self.x,
+            "y": self.y
+        }))) {
+            land = await game.spawn({
                 "classname": "land",
                 "x": self.x,
-                "y": self.y
-            }))) {
-                land = await game.spawn({
-                    "classname": "land",
-                    "x": self.x,
-                    "y": self.y,
-                    "height": 0
-                });
-            }
-
-            await land.update({
-                "height": land.height + Math.random()
+                "y": self.y,
+                "height": 0
             });
+        }
 
-            const moveChance = 1/3;
-            const shouldMove = Math.random() < moveChance;
-            if (shouldMove) {
-                const NORTH = 0;
-                const EAST = 1;
-                const SOUTH = 2;
-                const WEST = 3;
-                const DIRECTIONS = [NORTH, EAST, SOUTH, WEST];
+        await land.update({
+            "height": land.height + Math.random()
+        });
 
-                const moveDirection = oneOf(DIRECTIONS)
-                if (moveDirection === NORTH) {
-                    self.update({
-                        y: self.y+1
-                    });
-                } else if (moveDirection === EAST) {
-                    self.update({
-                        x: self.x+1
-                    });
-                } else if (moveDirection === SOUTH) {
-                    self.update({
-                        y: self.y-1
-                    });
-                } else if (moveDirection === WEST) {
-                    self.update({
-                        x: self.x-1
-                    });
-                }
+        const moveChance = 1/3;
+        const shouldMove = Math.random() < moveChance;
+        if (shouldMove) {
+            const NORTH = 0;
+            const EAST = 1;
+            const SOUTH = 2;
+            const WEST = 3;
+            const DIRECTIONS = [NORTH, EAST, SOUTH, WEST];
+
+            const moveDirection = oneOf(DIRECTIONS)
+            if (moveDirection === NORTH) {
+                self.update({
+                    y: self.y+1
+                });
+            } else if (moveDirection === EAST) {
+                self.update({
+                    x: self.x+1
+                });
+            } else if (moveDirection === SOUTH) {
+                self.update({
+                    y: self.y-1
+                });
+            } else if (moveDirection === WEST) {
+                self.update({
+                    x: self.x-1
+                });
             }
         }
     }
@@ -240,6 +175,8 @@ async function run() {
     let stepInterval = await ldClient.variation('step-interval', {key:"anonymous"}, 10000);
     let lastTick = currentTime - stepInterval;
     let lastSkipMessageTime = lastTick;
+
+    await think(self, game);
     while(true) {
         currentTime = Date.now();
         stepInterval = await ldClient.variation('step-interval', {key:"anonymous"}, 10000);
@@ -255,7 +192,8 @@ async function run() {
                 let data = snapshot.data();
                 const doesControllerExists = !!controllers[data.classname];
                 if (doesControllerExists) {
-                    controllers[data.classname](snapshot);
+                    const self = await makeEntity(snapshot.ref);
+                    controllers[data.classname](self, game);
                 } else {
                     await snapshot.ref.delete();
                 }
